db7d4e493e22ace962cc9a9207c19c0c776b5c63
JakubSzukala <burski09@outlook.com>
2025-05-10 15:32:53 +0200

    Animate player

--- src/player.gd
+++ src/player.gd
@@ -5,13 +5,45 @@ const CLIMB_TOP_SPEED = -400
 const CLIMB_SPEED = 50
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@export var _animation: AnimatedSprite2D
+
+enum FacingDirection
+{
+	LEFT,
+	RIGHT
+}
+
+var facing_direction: FacingDirection = FacingDirection.RIGHT
+
+
 func _physics_process(delta: float) -> void:
 	# Sideways movement
-	velocity.x = Input.get_axis("move_left", "move_right") * MOVE_SPEED * delta
+	var axis = Input.get_axis("move_left", "move_right")
+	velocity.x = axis * MOVE_SPEED * delta
 
-	# Apply gravity
+	if axis > 0:
+		facing_direction = FacingDirection.RIGHT
+	elif axis < 0:
+		facing_direction = FacingDirection.LEFT
+	#else: Nothing
+
 	if not is_on_floor():
+		# Apply gravity
 		velocity.y += GRAVITY * delta
+
+		# Play flight animation whilst we are in the air
+		_animation.play("fly")
+	else:
+		# If we are not, check if we have velocity.x different than 0
+		if velocity.x == 0.0:
+			_animation.play("idle")
+		else:
+			_animation.play("run")
+
+	if facing_direction == FacingDirection.RIGHT:
+		_animation.flip_h = true
+	else:
+		_animation.flip_h = false
 
 	# Fly
 	if Input.is_action_pressed("fly"):
