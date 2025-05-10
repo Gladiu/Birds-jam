d77e8cfb5b624f3197bf08e4a9ac461b1327299f
JakubSzukala <burski09@outlook.com>
2025-05-10 22:10:41 +0200

    Add enemy killing

--- src/poop.gd
+++ src/poop.gd
@@ -6,5 +6,12 @@ var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
-	$Sprite2D.rotation = velocity.angle() - 0.5 * PI # This offset is necessary
+	$Sprite2D.rotation = velocity.angle() - 0.5 * PI
 	move_and_slide()
+
+
+func _on_hitbox_body_entered(body):
+	var enemy = body as Enemy
+	if enemy:
+		enemy.queue_free()
+	queue_free()
