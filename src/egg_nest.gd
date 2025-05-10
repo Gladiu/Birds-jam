16a8f17f92821a8242340f3d168ccc6f78a39fb3
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2025-05-10 19:53:53 +0200

    Global state holder added

--- src/egg_nest.gd
+++ src/egg_nest.gd
@@ -2,13 +2,20 @@ extends Node2D
 class_name EggNest
 
 
-signal egg_destroyed;
+signal egg_destroyed(instance_id)
+signal egg_created(instance_id)
 
 
 func _ready():
-	pass
+	egg_created.connect(Global.add_egg_data)
+	egg_destroyed.connect(Global.remove_egg)
+	var egg_data: EggData = EggData.new()
+	egg_data.id = get_instance_id()
+	egg_data.position = global_position
+	egg_created.emit(egg_data)
 
 
 func destroy_egg():
+	var instance_id = get_instance_id()
 	queue_free()
-	egg_destroyed.emit()
+	egg_destroyed.emit(instance_id)
