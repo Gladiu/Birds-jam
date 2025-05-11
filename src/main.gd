97a668af7b3cbef6d9d3f26e749165d5360e3c04
JakubSzukala <burski09@outlook.com>
2025-05-11 19:06:51 +0200

    Destroy enemies at daytime

--- src/main.gd
+++ src/main.gd
@@ -8,12 +8,13 @@ var base_stick_spawn_count = 20
 var base_enemy_spawn_count = 4
 
 
-func _ready() -> void:
-	pass
-
-
 func _on_timer_60s_timeout():
 	$World.toggle_day_night()
+
+	# At day, destroy enemies
+	if $World.current_time == World.TIME_ENUM.DAY:
+		for enemy in find_children("*", "Enemy", true, false):
+			enemy.queue_free()
 
 	# Pick what to spawn, at night enemies at day sticks
 	var entity_to_spawn
@@ -38,7 +39,6 @@ func _on_timer_60s_timeout():
 
 
 func _process(delta):
-	print("screen size: ", DisplayServer.window_get_size())
 	var factor_untill_next_day = 1 - ($Timer60s.time_left / $Timer60s.wait_time)
 	$World.set_time_factor(factor_untill_next_day)
 	$Timer60s/Sky/Hand.rotation = factor_untill_next_day * 2 * PI
